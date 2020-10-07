1be34856eb49f1c5af88e1389ae1274a44cf5610
gmrokz <gpl31.msh@gmail.com>
2020-10-08 01:33:07 +0530

    Rest with JPa

--- restful-web-services/src/main/java/com/gpl3194/rest/webservices/restfulwebservices/user/User.java
+++ restful-web-services/src/main/java/com/gpl3194/rest/webservices/restfulwebservices/user/User.java
@@ -1,7 +1,12 @@
 package com.gpl3194.rest.webservices.restfulwebservices.user;
 
 import java.util.Date;
+import java.util.List;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 
@@ -9,8 +14,11 @@ import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 
 @ApiModel(description=" All the details about the user.")
+@Entity
 public class User {
 	
+	@Id
+	@GeneratedValue
 	private Integer id;
 	
 	@Size(min =2,message="Name should have atleast 2 character")
@@ -20,6 +28,8 @@ public class User {
 	@ApiModelProperty(notes=" Birth date should be in the past")
 	private Date birthDate;
 	
+	@OneToMany(mappedBy="user")
+	private List<Post> posts;
 
 	protected User() {
 		
@@ -54,6 +64,14 @@ public class User {
 
 	public void setBirthDate(Date birthDate) {
 		this.birthDate = birthDate;
+	}
+	
+	public List<Post> getPosts() {
+		return posts;
+	}
+
+	public void setPosts(List<Post> posts) {
+		this.posts = posts;
 	}
 
 	@Override
